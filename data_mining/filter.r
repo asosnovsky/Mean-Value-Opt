b438f64f368f7fb637c7343d4934dbe5bc359c41
Ari Sosnovsky <arielsosnovsky@gmail.com>
2015-11-22 19:37:59 -0500

    modified filtering alogrithm

--- data_mining/filter.r
+++ data_mining/filter.r
@@ -4,6 +4,9 @@ library(dplyr)
 library(timeSeries)
 library(zoo)
 
+#set home directory if needed
+#setwd("F:/Mean-Value-Opt/data_mining")
+
 getDateVector <- function(d1,d2,format="%Y%m%d") {
   dates <-seq.Date(to = as.Date(d2,format=format),
                    from = as.Date(d1,format=format),
@@ -20,13 +23,15 @@ partitions_year = rbind(c("20080101","20081231"),
                         c("20110101","20111231"),
                         c("20120101","20121231"))
 
-for (sector in sector_html_titles[1])
+#for (sector in sector_html_titles)
+sector = sector_html_titles[1];
 {
-  stock_dir <- paste0("data/time_series/2008-2012/",sector)
+  stock_dir <- paste0("time_series/2008-2012/",sector)
   stock_list <- gsub(".csv","",dir(stock_dir))
-  dir.create("data/filtered_stocks/",showWarnings = FALSE)
+  dir.create("filtered_stocks/",showWarnings = FALSE)
 
-    for(part in 1:length(partitions_year[,1]))
+    #for(part in 1:length(partitions_year[,1]))
+    part = 1
     {
       # create general date vector
       merged_data <- data.frame(Index=getDateVector(partitions_year[part,1],partitions_year[part,2]))
@@ -34,7 +39,7 @@ for (sector in sector_html_titles[1])
       year <- substr(partitions_year[part,1],1,4)
       
       #create directory name data/filtered_stocks/YEAR
-      merged_data_directory <- paste0("data/filtered_stocks/",year)
+      merged_data_directory <- paste0("filtered_stocks/",year)
       dir.create(merged_data_directory,showWarnings = FALSE)
       
       #create file name data/filtered_stocks/YEAR/SECTOR.csv
@@ -46,38 +51,32 @@ for (sector in sector_html_titles[1])
         data[,1] <- as.Date(data[,1]);
         # filter data by year
         data_year <- dplyr::filter(data,grepl(year,Index))
+        
         # coerce index to date and close prices to numeric
         data_year$Index <- as.Date(data_year$Index)
         data_year$Close <- as.numeric(data_year$Close)
+        
         # rename col.names to specific stock
         data_year_short <- data_year[c("Index","Close")]
         colnames(data_year_short) <- c("Index",paste0("Close.",stock))
         
-        if (nrow(data_year) > nrow(date_vector)*.85) #if the stock has more than 85% of time data we keep it
+        if (nrow(data_year) > nrow(merged_data)*.85) #if the stock has more than 85% of time data we keep it
         {
           merged_data <- merge(merged_data,data_year_short,by.x = "Index",by.y = "Index",all.x = TRUE)
           # determine if last column of merged_data had NA values if so replace with previous open price. 
           
           last.na = 0;
-          for (naInd in which(is.na(merged_data[,ncol(merged_data)]))) {
-            if (naInd == 1)
-            {
-              #if first day is NA skip. 
-              next
+          for (naInd in which(is.na(merged_data[,ncol(merged_data)])) ) {
+            possibs = which(data[,1] < merged_data[naInd,1]);
+            if( length(possibs) > 0 ) {
+              merged_data[naInd,ncol(merged_data)] = data[max(possibs),4];
+            } else {
+              merged_data[naInd,ncol(merged_data)] = data[min(which(data[,1] >= merged_data[naInd,1])),2];
             }
-            else if( last.na+1 == naInd ) 
-            {
-              merged_data[,ncol(merged_data)][naInd] = merged_data[,ncol(merged_data)][last.na]
-            }  
-            else 
-            {
-              merged_data[,ncol(merged_data)][naInd] = data[data[,1] == merged_data$Index[naInd-1],2]
-            }
-            
-            last.na = naInd;
           }
           
         }
+        print(paste0(round(which(stock_list == stock)/length(stock_list)*100,2),'% Done'));
         write.csv(merged_data,file = merged_data_file,row.names = FALSE)
       }
       
