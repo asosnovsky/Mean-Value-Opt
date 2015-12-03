294cfe998464c2577add0aab7441b22fd3bc6dac
kChhin <kosalchhin@gmail.com>
2015-12-03 15:56:27 -0500

    Take a look at the condensed report
    Obviously need some more parts

---
+++ analysis/optimizeSharpPortee.m
@@ -0,0 +1,60 @@
+function [ sharpe, P, Wp, sharpes, mps, risks ] = optimizeSharpPortee( Ret, CoRisk, rfr, portlim, NegOrPos, SUPPMSG )
+%optimizeSharpPort Optimize a portfolio based on a list of expected returns and
+%the covariance matrix. Using the MVP method adn Sharpe-minimization.
+
+if (nargin < 6 || ~SUPPMSG)
+    SUPPMSG = false;
+else
+    SUPPMSG = true;
+end
+% Select a starting point for the portfolio
+P = [];
+Wp = [];
+
+% Helper
+sharpe = @(P,Wp,rfr) (Ret(P)*Wp-rfr)/sqrt(Wp'*CoRisk(P,P)*Wp);
+
+mps = [0];
+risks = [0];
+limmp = [max([min(Ret(Ret>0)) rfr]) max(Ret)]; 
+clc
+sharpes = [Inf 0];
+c = 2;
+if(~SUPPMSG); disp('Initializing.... (first computation takes more time)');end;
+function [] = optimo()
+    m0 = limmp(1);
+    mk = limmp(2);
+    while abs(sharpes(c)-sharpes(c-1))> 1E-25
+        ud = [ mk<m0 mk>m0 ];%if mk > m0 then limmp([0 1])=max else limmp([1 0])=min
+        m0 = median(limmp);
+        mk = median([m0 limmp(ud)]);
+                                
+        [P, Wp] = optimizePortee( Ret, CoRisk, m0, portlim, NegOrPos, P, Wp  );
+        sharpes(c) = sharpe(P,Wp,rfr);
+        [P, Wp, lRisk] = optimizePortee( Ret, CoRisk, mk, portlim, NegOrPos, P, Wp  );
+        
+        sharpes(c+1) = sharpe(P,Wp,rfr);
+        mps(c) = mk;
+        risks(c) = lRisk;
+        if(sharpes(c+1) < sharpes(c))
+            limmp(ud) = mk;
+        else
+            limmp(~ud) = m0;
+        end
+        c = c+1;
+        % <Text>
+        if ~SUPPMSG
+            %clc
+            fprintf('\n> (%d) %2.7f\n', length(sharpes(2:end)) ,sharpes(end));
+            fprintf('   mp: %2.7f in [%2.7f,%2.7f] \n', mk, limmp(1), limmp(2)); 
+            fprintf('   risk: %2.7f\n', risks(end));
+        end
+        % <Text> 
+    end
+end
+optimo();
+sharpe = sharpes(end);
+sharpes = sharpes(2:end);
+if(~SUPPMSG); disp('[ << DONE >> ]'); end;
+end
+
